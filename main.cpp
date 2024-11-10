46e06988063715d5c6ab3d147985716cfac637d0
Никита <ganchukovnikita@bk.ru>
2024-11-10 14:03:18 +0700

    сделал возможность читать чанки информации из картинки

--- main.cpp
+++ main.cpp
@@ -1,54 +1,61 @@
 
+#include <algorithm>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
 
-
-
-
-struct chunk
+class chunk
 {
-    explicit chunk(const int32_t len, const int16_t startPos)
+private:
+    int32_t beginPos;
+    int32_t endPos;
+    int32_t length;
+
+    char* descriptionChunk;
+    char type[4];
+    char crc[4];
+
+public:
+    explicit chunk(const int32_t len, const int32_t startPos)
         : beginPos(startPos), endPos(startPos + len + 4 * 2)
-        , lenght(len), type{}, crc{}
+        , length(len), type{}, crc{}
     {
         descriptionChunk = new char[len];
     }
 
     chunk(const chunk& other)
-        : beginPos(other.beginPos)
-      , endPos(other.endPos)
-      , lenght(other.lenght)
-      , descriptionChunk(new char[other.lenght])
+        : beginPos(other.beginPos), endPos(other.endPos), length(other.length)
+        , type{}, descriptionChunk(new char[other.length]), crc{}
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < other.length; i++)
         {
             type[i] = other.type[i];
             crc[i] = other.crc[i];
         }
 
         std::copy(other.descriptionChunk,
-            other.descriptionChunk + other.lenght, descriptionChunk);
+            other.descriptionChunk + other.length, descriptionChunk);
     }
-
     chunk(chunk&& other) noexcept
-        : beginPos(other.beginPos)
-      , endPos(other.endPos)
-      , lenght(other.lenght)
-      , descriptionChunk(other.descriptionChunk)
+        : beginPos(other.beginPos), endPos(other.endPos), length(other.length)
+        , type{}, descriptionChunk(other.descriptionChunk), crc{}
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < other.length; i++)
         {
             type[i] = other.type[i];
             crc[i] = other.crc[i];
         }
 
-        other.descriptionChunk = nullptr;
-        other.lenght = 0;
-        other.crc = {};
-        other.type = {};
+        for (int i = 0; i < other.length; i++)
+        {
+            other.type[i] = 0;
+            other.crc[i] = 0;
+        }
+
         other.beginPos = 0;
         other.endPos = 0;
+        other.descriptionChunk = nullptr;
+        other.length = 0;
     }
 
     chunk& operator=(const chunk& other)
@@ -56,7 +63,7 @@ struct chunk
         if (this == &other)
             return *this;
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < other.length; i++)
         {
             type[i] = other.type[i];
             crc[i] = other.crc[i];
@@ -65,45 +72,121 @@ struct chunk
         delete[] descriptionChunk;
         beginPos         = other.beginPos;
         endPos           = other.endPos;
-        lenght           = other.lenght;
-        descriptionChunk = new char[other.lenght];
+        length           = other.length;
+        descriptionChunk = new char[other.length];
         std::copy(other.descriptionChunk,
-            other.descriptionChunk + other.lenght, descriptionChunk);
+            other.descriptionChunk + other.length, descriptionChunk);
         return *this;
     }
-
     chunk& operator=(chunk&& other) noexcept
     {
         if (this == &other)
             return *this;
 
-        for (int i = 0; i < 4; i++)
+        delete[] descriptionChunk;
+        beginPos         = other.beginPos;
+        endPos           = other.endPos;
+        length           = other.length;
+        descriptionChunk = other.descriptionChunk;
+
+        for (int i = 0; i < other.length; i++)
         {
             type[i] = other.type[i];
             crc[i] = other.crc[i];
         }
 
-        delete[] descriptionChunk;
-        beginPos         = other.beginPos;
-        endPos           = other.endPos;
-        lenght           = other.lenght;
-        descriptionChunk = other.descriptionChunk;
+        for (int i = 0; i < other.length; i++)
+        {
+            other.type[i] = 0;
+            other.crc[i] = 0;
+        }
+
+        other.descriptionChunk = nullptr;
+        other.length = 0;
+        other.beginPos = 0;
+        other.endPos = 0;
+
         return *this;
     }
 
-    int32_t beginPos;
-    int32_t endPos;
-    int32_t lenght;
-    char type[4];
-    char* descriptionChunk;
-    char crc[4];
-
     ~chunk()
     {
+        if (!descriptionChunk)
+            return;
+
         delete[] descriptionChunk;
+    }
+
+    enum class typeBlock : uint16_t
+    {
+        TYPE = 1,
+        CRC,
+        DESCRIPTION,
+    };
+
+    [[nodiscard]] int32_t getBeginPos() const
+    {
+        return beginPos;
+    }
+    [[nodiscard]] int32_t getEndPos() const
+    {
+        return endPos;
+    }
+    [[nodiscard]] int32_t getLength() const
+    {
+        return length;
+    }
+
+    [[nodiscard]] char* getDescriptionChunk() const
+    {
+        return descriptionChunk;
+    }
+    [[nodiscard]] char* getType()
+    {
+        return type;
+    }
+    [[nodiscard]] char* getCrc()
+    {
+        return crc;
+    }
+
+    static void templatePrint(const char* arr, const int32_t len)
+    {
+        for (auto i = arr; i < &arr[len]; i++)
+            std::cout << std::hex << static_cast<int16_t>(*i) << ' ';
+        std::cout << '\n';
+    }
+
+    void print(const typeBlock& type) const
+    {
+        switch (type)
+        {
+            case typeBlock::TYPE:
+            {
+                std::cout << "Type (name) - ";
+                templatePrint(this->type, 4);
+                break;
+            }
+            case typeBlock::CRC:
+            {
+                std::cout << "CRC - ";
+                templatePrint(crc, 4);
+                break;
+            }
+            case typeBlock::DESCRIPTION:
+            {
+                std::cout << "data chunk - ";
+                templatePrint(descriptionChunk, length);
+                break;
+            }
+        }
     }
 };
 
+class FormatPng
+{
+
+};
 
 static int32_t size_file(std::ifstream& file)
 {
@@ -114,15 +197,15 @@ static int32_t size_file(std::ifstream& file)
     return static_cast<int32_t>(end - begin - 20);
 }
 
-static int32_t convertCharInInt(char lenght[4])
+static int32_t convertCharInInt(char length[4])
 {
-    return std::stoi(std::to_string(lenght[0]) +
-                        std::to_string(lenght[1]) +
-                        std::to_string(lenght[2]) +
-                        std::to_string(lenght[3]));
+    return std::stoi(std::to_string(length[0]) +
+                        std::to_string(length[1]) +
+                        std::to_string(length[2]) +
+                        std::to_string(length[3]));
 }
 
-static int32_t calcLengthChunk(std::ifstream& file, int& pos)
+static int32_t calcLengthChunk(std::ifstream& file, int32_t& pos)
 {
     static constexpr int8_t size = 4;
     char len[size];
@@ -132,44 +215,29 @@ static int32_t calcLengthChunk(std::ifstream& file, int& pos)
     return convertCharInInt(len);
 }
 
-chunk createChunk(std::ifstream& file, int pos)
+void read(std::ifstream& file, char* arr, int32_t& pos, const int32_t size)
+{
+    file.read(arr, size);
+    file.seekg(pos += size);
+}
+
+chunk createChunk(std::ifstream& file, int32_t& pos)
 {
     chunk header(calcLengthChunk(file, pos), pos);
 
-    std::cout << "Length header - " << header.lenght << '\n';
-
-    file.read(header.type, 4);
-    file.seekg(16);
-    std::cout << "Type (name) - ";
-    for (const auto i : header.type)
-        std::cout << std::hex << static_cast<int16_t>(i) << ' ';
-    std::cout << '\n';
-
-    file.read(header.descriptionChunk, header.lenght);
-    file.seekg(16 + header.lenght);
-
-    std::cout << "data chunk - ";
-    for (int i = 0; i < header.lenght; i++)
-        std::cout << std::hex << static_cast<int16_t>(header.descriptionChunk[i]) << ' ';
-    std::cout << '\n';
-
-    file.read(header.crc, 4);
-    file.seekg(20 + header.lenght);
-
-    std::cout << "CRC - ";
-    for (const auto i : header.crc)
-        std::cout << std::hex << static_cast<int16_t>(i) << ' ';
-    std::cout << '\n';
+    read(file, header.getType(), pos, 4);
+    read(file, header.getDescriptionChunk(), pos, header.getLength());
+    read(file, header.getCrc(), pos, 4);
 
     return header;
 }
+
 
 int main()
 {
     // signator PNG
     int pos = 8;
     char signator[8];
-    char lenght[4];
 
 
     std::ifstream img("data.png", std::ifstream::binary | std::ifstream::in);
@@ -181,28 +249,19 @@ int main()
 
     img.read(signator, pos);
 
-    chunk header = createChunk(img, pos);
+    const chunk header = createChunk(img, pos);
+    header.print(chunk::typeBlock::TYPE);
+    header.print(chunk::typeBlock::DESCRIPTION);
+    header.print(chunk::typeBlock::CRC);
+    std::cout << "------\n";
+    const chunk srgb = createChunk(img, pos);
+    srgb.print(chunk::typeBlock::TYPE);
+    srgb.print(chunk::typeBlock::DESCRIPTION);
+    srgb.print(chunk::typeBlock::CRC);
 
-    std::cout << "Type (name) - ";
-    for (const auto i : header.type)
-        std::cout << std::hex << static_cast<int16_t>(i) << ' ';
-    std::cout << '\n';
-    
-    std::cout << "data chunk - ";
-    for (int i = 0; i < header.lenght; i++)
-        std::cout << std::hex << static_cast<int16_t>(header.descriptionChunk[i]) << ' ';
-    std::cout << '\n';
-
-    std::cout << "CRC - ";
-    for (const auto i : header.crc)
-        std::cout << std::hex << static_cast<int16_t>(i) << ' ';
-    std::cout << '\n';
+    std::cout << std::dec << "pos " << pos << '\n';
 
     img.close();
-
-    for (const auto i : signator)
-        std::cout << std::hex << static_cast<int16_t>(i) << ' ';
-    std::cout << '\n';
 
     return 0;
 }
